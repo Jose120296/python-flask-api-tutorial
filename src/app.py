dc857d1513be6039ca95ed16c6b062c4cc38408c
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-01-10 15:35:52 +0000

    ejercicio 8

--- src/app.py
+++ src/app.py
@@ -20,5 +20,10 @@ def add_new_todo():
     todos.append(request_body)
     return jsonify(todos)
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3245, debug=True)
